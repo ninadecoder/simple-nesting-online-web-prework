269849168576a7b74b32d8c2656be288cfa68fdb
Nina Kazzim <kazzim@icloud.com>
2019-06-26 11:57:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
   
-  programmer_hash = {:dennis_ritchie => {:known_for => "Unix", :languages => ["C", "Assembly"]}}
+  return programmer_hash
+  
+  #programmer_hash = {:dennis_ritchie => {:known_for => "Unix", :languages => ["C", "Assembly"]}}
 
 end
